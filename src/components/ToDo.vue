f0a9237ed9daa170ee5b71690a3bd4e354e1a7c4
andreyromanov <andreygorogogo@gmail.com>
2019-02-27 12:11:27 +0200

    Delete function

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -4,8 +4,8 @@
     <input @keyup.enter="add" type="text" v-model="title" placeholder="Task to do ...">
     <button @click="add">Add task</button>
 
-    <ul>
-        <li v-for="todo in todos">{{todo}}</li>
+    <ul>  
+        <li v-for="(todo,index) in todos">{{todo}} <span @click="del(index)" class="delete">&nbsp;X&nbsp;</span></li>
     </ul>
   </div>
 </template>
@@ -20,22 +20,38 @@ export default {
   data(){
       return{
           title: '',
-          todos: [
-              'Go to ...'
-          ]
+          todos: []
       }
   },
   
   methods: {
       add(){
-          this.todos.push(this.title);
+        if(this.title != ''){
+          this.todos.unshift(this.title);
           this.title = "";
+        }
+      },
+      del(id){
+        this.$delete(this.todos, id);
       }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
+<style>
+.delete{
+  padding: 5px;
+  border-radius: 50%;
+  background-color: red;
+}
+.delete:hover{
+  cursor: pointer;
+}
+ul{
+  list-style-type: none;
+}
+li{
+  margin-top: 15px;
+}
 </style>
